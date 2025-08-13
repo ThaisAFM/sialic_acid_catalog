09973bc6d7e3852df09cb6d774f45b971246b2d7
ThaisAFM <thaisalanaf@usp.br>
2025-08-13 13:56:34 -0300

    Update nr100.sh

--- CDHIT/nr100.sh
+++ CDHIT/nr100.sh
@@ -9,3 +9,12 @@ for arquivo in *.fasta; do
     cd-hit -i "$arquivo" -o "$saida" -c 1.00 -n 5 -M 2000
     echo "Processed: $arquivo -> ${saida}"
 done
+
+# Organazing the files:
+# Move all non-redundant FASTA files to a dedicated folder
+# and rename them to simplify the next steps
+
+mkdir fastas_nr
+cp *nr100 fastas_nr
+cd fastas_nr
+rename 's/_nr100$/.fasta/' * 
