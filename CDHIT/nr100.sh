91f99e7b1fbf8fb26dd1a8cf0818f7b1f2c0a2f6
ThaisAFM <thaisalanaf@usp.br>
2025-08-13 13:48:30 -0300

    Update nr100.sh

--- CDHIT/nr100.sh
+++ CDHIT/nr100.sh
@@ -1,11 +1,9 @@
-### Running CD-HIT for All FASTA Files
+# Iterate through all FASTA files and run CD-HIT at 100% identity
 
-# The following command iterates through all FASTA files in the current folder and applies the **CD-HIT** clustering algorithm to each one.  
 # It generates:  
 # A `.clstr` file containing the clusters of redundant sequences at **100% identity**.  
 # A FASTA file containing only the representative sequence for each cluster.
 
-```bash
 for arquivo in *.fasta; do
     saida="${arquivo%.fasta}_nr100"
     cd-hit -i "$arquivo" -o "$saida" -c 1.00 -n 5 -M 2000
