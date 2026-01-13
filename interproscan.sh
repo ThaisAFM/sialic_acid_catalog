de15d1cedd20450f86737bf3f195761c848183a0
ThaisAFM <thaisalanaf@usp.br>
2026-01-13 15:30:53 -0300

    Create interproscan.sh

---
+++ interproscan.sh
@@ -0,0 +1,5 @@
+# InterProScan v5.76 was installed according to the official documentation,
+# available at: https://interproscan-docs.readthedocs.io/en/v5/HowToDownload.html,
+# and was executed locally
+
+interproscan.sh -i <filename>.fasta -o <filename>_interpro -f tsv -dp
